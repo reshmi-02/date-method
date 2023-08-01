bb54b92fa29ca90930f0e7da4caf61627c6586d7
reshmi-02 <reshmihash123@gmail.com>
2023-08-01 19:51:36 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,70 @@
+var date=new Date();
+console.log("1)new Date");
+console.log(date);
+document.write("1)new Date<br>");
+document.write(date+"<br><br>");
+
+
+var a=date.getFullYear();
+console.log("2)getFullYear");
+console.log(a);
+document.write("2)getFullYear<br>");
+document.write(a+"<br><br>");
+
+
+var a=date.getMonth();
+console.log("3)getMonth");
+console.log(a);
+document.write("3)getMonth<br>");
+document.write(a+"<br><br>");
+
+
+var a=date.getDate();
+console.log("4)getDate");
+console.log(a);
+document.write("4)getDate<br>");
+document.write(a+"<br><br>");
+
+
+var a=date.getDay();
+console.log("5)getDay");
+console.log(a);
+document.write("5)getDay<br>");
+document.write(a+"<br><br>");
+
+
+
+var a=date.getHours();
+console.log("6)getHours");
+console.log(a);
+document.write("6)getHours<br>");
+document.write(a+"<br><br>");
+
+
+var a=date.getMinutes();
+console.log("7)getMinutes");
+console.log(a);
+document.write("7)getMinutes<br>");
+document.write(a+"<br><br>");
+
+
+var a=date.getSeconds();
+console.log("8)getSeconds");
+console.log(a);
+document.write("8)getSeconds<br>");
+document.write(a+"<br><br>");
+
+
+
+var a=date.getMilliseconds();
+console.log("9)getMilliseconds");
+console.log(a);
+document.write("9)getMilliseconds<br>");
+document.write(a+"<br><br>");
+
+
+var a=date.getTime();
+console.log("10)getTime");
+console.log(a);
+document.write("10)getTime<br>");
+document.write(a+"<br><br>");
